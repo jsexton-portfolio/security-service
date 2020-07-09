23f57b886d6d6a1ccbddd5da3d4ef8a9be72ac21
JSextonn <justinsexton.dev@gmail.com>
2020-07-09 01:00:56 -0500

    Added comment explaining regex

--- chalicelib/form.py
+++ chalicelib/form.py
@@ -28,7 +28,12 @@ class PasswordUpdateForm(BaseModel):
     @validator('new_password')
     def password_validator(cls, value):
         # Regex representing cognito's password policy
-        #
+        # At least one lower case letter
+        # At least one upper case letter
+        # At least one digit
+        # At least one special character
+        # Minimum length of 8
+        # Maximum length of 99
         regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\^$*.\[\]{}\(\)?\-“!@#%&/,><\’:;|_~`])\S{8,99}$')
         if not regex.match(value):
             raise ValueError('password does not conform to policy')
